377e9a117515026dc24016aa2afaf2a0328a9247
Gergo <gege251@gmail.com>
2018-11-02 17:35:11 +0900

    js file optimization

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = Elm.Main.init({
 })
 
 const soundEffects = {
-  fire: new Audio("./assets/audio/1.wav"),
-  explode: new Audio("./assets/audio/2.wav"),
-  hit: new Audio("./assets/audio/3.wav"),
-  ufoStep1: new Audio("./assets/audio/4.wav"),
-  ufoStep2: new Audio("./assets/audio/5.wav"),
-  ufoStep3: new Audio("./assets/audio/6.wav"),
-  ufoStep4: new Audio("./assets/audio/7.wav")
+  fire: "./assets/audio/1.wav",
+  explode: "./assets/audio/2.wav",
+  hit: "./assets/audio/3.wav",
+  ufoStep1: "./assets/audio/4.wav",
+  ufoStep2: "./assets/audio/5.wav",
+  ufoStep3: "./assets/audio/6.wav",
+  ufoStep4: "./assets/audio/7.wav"
 }
 
-app.ports.soundEffect.subscribe(name => soundEffects[name].play())
+app.ports.soundEffect.subscribe(name => new Audio(soundEffects[name]).play())
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 
 module.exports = (_env, args) => ({
   entry: path.resolve(__dirname, "index.js"),
@@ -16,6 +17,16 @@ module.exports = (_env, args) => ({
         }
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
         test: /\.html$/,
         exclude: /node_modules/,
         use: {
@@ -26,6 +37,39 @@ module.exports = (_env, args) => ({
           }
         }
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            pure_funcs: [
+              "F2",
+              "F3",
+              "F4",
+              "F5",
+              "F6",
+              "F7",
+              "F8",
+              "F9",
+              "A2",
+              "A3",
+              "A4",
+              "A5",
+              "A6",
+              "A7",
+              "A8",
+              "A9"
+            ],
+            pure_getters: true,
+            keep_fargs: false,
+            unsafe_comps: true,
+            // unsafe: true,
+            passes: 3
+          }
+        }
+      })
     ]
   },
   plugins: [new CopyWebpackPlugin([{ from: "./assets/audio/*.wav", to: "" }])]
